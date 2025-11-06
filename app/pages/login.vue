58cc7b0d9360d406d087aaa35da1fb224c8492ab
Arif Yahya <arif.yahya@gajicermat.com>
2025-11-06 20:51:08 +0700

    add: as user i want to have login page

--- app/pages/login.vue
+++ app/pages/login.vue
@@ -1,26 +1,24 @@
 <script setup lang="ts">
 import type { AuthFormField } from '@nuxt/ui'
 
-definePageMeta({
-    layout: 'blank'
-})
+definePageMeta({ layout: 'blank' })
 
 const fields = ref<AuthFormField[]>([
-    {
-        name: 'email',
-        type: 'text',
-        label: 'Email'
-    },
-    {
-        name: 'password',
-        type: 'password',
-        label: 'Password'
-    }
+    { name: 'email', type: 'text', label: 'Email', required: true },
+    { name: 'password', type: 'password', label: 'Password', required: true }
 ])
+
+const { login } = useAuth()
+
+const onSubmit = async (event: any) => {
+    await login(event.data.email, event.data.password)
+    navigateTo('/')
+}
 </script>
 
 <template>
     <div class="min-h-screen flex items-center justify-center">
-        <UAuthForm title="Login" :fields="fields" class="max-w-md w-full" />
+        <UAuthForm title="Login" :fields="fields" :submit="{ label: 'Login' }" @submit="onSubmit"
+            class="max-w-md w-full" />
     </div>
 </template>
